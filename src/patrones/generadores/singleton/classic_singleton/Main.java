8483232bcfc4516055fa7f9c567694900b912898
Ander Raso Vázquez <anderraso@gmail.com>
2018-03-18 12:03:04 +0100

    main añadido para explicar el funcionamiento del singleton
    Signed-off-by: Ander Raso Vázquez <anderraso@gmail.com>

---
+++ src/patrones/generadores/singleton/classic_singleton/Main.java
@@ -0,0 +1,12 @@
+package patrones.generadores.singleton.classic_singleton;
+
+public class Main {
+
+    public static void main(String[] args){
+        // la forma de llamarlo es la siguiente
+        Singleton.getInstancia();
+
+        // si tuviera mas metodos se llaman de esta forma
+        //Singleton.getInstancia().suMetodo()
+    }
+}
